3cedfcbe73e0ac651c350dd62dd98f18ac807040
erikvm <erik.vainumae@gmail.com>
2025-04-29 23:14:08 +0300

    implemented crud operations using mediator

---
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Activities.Commands;
+
+public class DeleteActivity
+{
+    
+}
---
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Activities.Queries;
+
+public class GetActivityDetails
+{
+    
+}
