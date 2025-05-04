82f72f0436e597490dd9c0b936c2e901787e37cb
erikvm <erik.vainumae@gmail.com>
2025-05-04 22:53:09 +0300

    created and styled navbar, added dashboard, view and for for a activity

---
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,30 @@
+import { Grid } from "@mui/material";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+type Props = {
+  activities: Activity[];
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  selectedActivity?: Activity;
+};
+
+export default function ActivityDashboard({ activities, selectActivity, cancelSelectActivity, selectedActivity }: Props) {
+  return (
+    <Grid container spacing={3}>
+      <Grid size={7}>
+        <ActivityList 
+        activities={activities} 
+        selectActivity = {selectActivity}
+        />
+      </Grid>
+      <Grid size={5}>
+        {selectedActivity && <ActivityDetails activity={selectedActivity} 
+        cancelSelectActivity = {cancelSelectActivity}
+        />}
+        <ActivityForm></ActivityForm>
+      </Grid>
+    </Grid>
+  );
+}
