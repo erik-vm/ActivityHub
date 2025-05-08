e804f9d3d31f75e022947f02a79cf00117d64ea6
erikvm <erik.vainumae@gmail.com>
2025-05-08 21:43:28 +0300

    end of section 6. installed and implemented react query

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -7,14 +7,16 @@ import {
   Chip,
   Typography,
 } from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
   activity: Activity;
   selectActivity: (id: string) => void;
-  deleteActivity: (id:string) => void;
 };
 
-export default function ActivityCard({ activity, selectActivity, deleteActivity }: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
+  const { deleteActivity } = useActivities();
+
   return (
     <Card sx={{ borderRadius: 3 }}>
       <CardContent>
@@ -31,15 +33,24 @@ export default function ActivityCard({ activity, selectActivity, deleteActivity 
         sx={{ display: "flex", justifyContent: "space-between", pb: 2 }}
       >
         <Chip label={activity.category} variant="outlined" />
-        <Box sx={{display: "flex", gap: 1}}>
-        <Button onClick={()=> selectActivity(activity.id)} size="medium" variant="contained">
-          View
-        </Button>
-        <Button color="error" onClick={()=> deleteActivity(activity.id)} size="medium" variant="contained">
-          Delete
-        </Button>
+        <Box sx={{ display: "flex", gap: 1 }}>
+          <Button
+            onClick={() => selectActivity(activity.id)}
+            size="medium"
+            variant="contained"
+          >
+            View
+          </Button>
+          <Button
+            color="error"
+            onClick={() => deleteActivity.mutate(activity.id)}
+            disabled={deleteActivity.isPending}
+            size="medium"
+            variant="contained"
+          >
+            Delete
+          </Button>
         </Box>
-   
       </CardActions>
     </Card>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './app/layouts/styles.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import App from './app/layouts/App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./app/layouts/styles.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import App from "./app/layouts/App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
